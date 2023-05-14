78e19d89d2bf6e302a910067f0b39bfbdfdb5c2b
Cassie Cheung <me@soopy.moe>
2023-05-15 02:49:22 +0800

    fix: main page changes
    * removed all bloat
    * added different video sources
    * [TODO] add video overlay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,64 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { Player, Youtube } from '@vime/svelte';
-    let video: HTMLDivElement;
-    
-    onMount(() => {
-        const test: HTMLImageElement = document.createElement('img')
-        test.src = 'https://upload.wikimedia.org/wikipedia/commons/c/c4/PM5544_with_non-PAL_signals.png'
-        test.classList.add('w-full')
-        video.appendChild(test);
-    });
-
+    // import { onMount } from 'svelte';
 </script>
 
-<div>
-    <div id="bg-video" class="-z-50" bind:this={video}>
-        <Player>
-            <Youtube videoId="_RSzENqfk04" showFullScreenControl={false}/>
-        </Player>
+<div class="w-full">
+    <div id="background-video" class="">
+        <video class="" muted disablepictureinpicture autoplay loop preload="metadata">
+            <source
+                src="https://assets.soopy.moe/tmp/SPKC_2023-ABDAY-SNIP-5e1182d4-ee33-11ed-b353-000d3ac6c367.webm"
+                type="video/webm"
+            />
+            <source
+                src="https://assets.soopy.moe/tmp/SPKC_2023-ABDAY-SNIP-ad268605-f607-44e0-b559-466e4efdeef4.mp4"
+                type="video/mp4"
+            />
+            <p>
+                Weird. Your browser couldn't play any of the video source files. Please consider filing a bug report on
+                https://github.com/soopyc/2024-sba or contact me at mailto:me@soopy.moe
+            </p>
+        </video>
+    </div>
+
+    <div id="hero-text" class="">
+        <p>Insert word here</p>
     </div>
 </div>
+
+<div>
+    <!-- <p>Esse ea sint culpa tempor. Ullamco culpa anim exercitation eu cupidatat exercitation ad. Cupidatat aute enim adipisicing tempor veniam anim nisi esse est id aute ullamco mollit.
+
+Non aliqua in officia occaecat officia irure Lorem anim velit nulla veniam ipsum commodo. Elit qui ipsum magna enim ipsum duis irure sunt. Occaecat magna aliqua Lorem culpa do aute officia magna excepteur ipsum nostrud pariatur.
+
+Reprehenderit adipisicing ipsum duis laborum magna labore amet exercitation cupidatat mollit enim sint pariatur amet. Dolor pariatur reprehenderit deserunt labore voluptate consectetur dolor sint pariatur. Fugiat nostrud incididunt culpa aliquip reprehenderit proident et aute irure aliqua labore ea ad. Ipsum incididunt reprehenderit deserunt pariatur et.
+
+Mollit Lorem eu pariatur ut consectetur in adipisicing irure. Nisi ullamco ut sit commodo incididunt irure adipisicing exercitation culpa excepteur reprehenderit irure velit. Non ad dolor est laborum consequat consequat nostrud sunt excepteur nostrud laboris sint. Veniam laboris sit ad exercitation aliquip sit.
+
+Consequat irure ipsum pariatur magna excepteur et culpa enim minim sunt. Irure eu exercitation consectetur ipsum nulla dolore dolor excepteur id. Nulla dolore occaecat ipsum fugiat eiusmod.
+
+Aute esse tempor eiusmod reprehenderit. Id sunt do culpa id dolore officia minim esse veniam quis. Ex consectetur adipisicing ullamco commodo non tempor voluptate consequat cillum laboris. Dolore enim qui occaecat cupidatat commodo officia cillum nisi Lorem voluptate. Amet consectetur minim enim voluptate dolor occaecat.
+
+Et eu quis aliquip consequat enim ipsum ut do ullamco ad ad. Anim quis non nisi et minim do exercitation ad irure consectetur occaecat ut. Consectetur veniam quis et deserunt sint Lorem.
+
+Cillum tempor do veniam sunt. Eiusmod aliquip dolor cupidatat laborum in ex reprehenderit ea. Nisi ipsum laboris voluptate do exercitation do enim enim ad ullamco est minim voluptate eiusmod. Eu sint cillum pariatur enim qui in nostrud Lorem occaecat qui deserunt eu. Tempor commodo fugiat minim magna. Aliqua fugiat aliqua ad ea amet enim ea sint aliqua fugiat tempor tempor. Laboris mollit sit id et esse ea esse irure eiusmod nisi sunt nostrud do.
+
+Ut ad fugiat esse non ex. Occaecat amet sint incididunt pariatur veniam est pariatur exercitation eiusmod consequat dolor sint ad sint. Laboris enim deserunt incididunt ut elit ut officia do occaecat id. Velit dolor ullamco non exercitation pariatur ullamco pariatur. Exercitation dolore aliqua exercitation consequat cupidatat consectetur elit adipisicing.
+
+Ut culpa commodo veniam ut consectetur do fugiat magna adipisicing fugiat veniam ea enim. Aliquip amet commodo minim labore nulla velit. Aliquip duis et aliqua eu in esse ea. Labore mollit est ut et consequat do commodo. Eiusmod aliqua sit sunt dolor magna ea voluptate deserunt nisi veniam amet voluptate ad eu. Fugiat laboris non anim et est amet laboris ad anim esse.
+
+Aliquip sunt ipsum fugiat fugiat elit qui labore id officia. Incididunt esse nostrud Lorem pariatur dolore anim ea fugiat consequat velit velit nisi occaecat exercitation. Est irure id mollit id quis irure eiusmod laborum. Laboris ex id nisi ex dolor deserunt eiusmod. Occaecat in eiusmod sit cupidatat nostrud. Fugiat Lorem et consectetur tempor culpa in eiusmod eiusmod ullamco proident laboris consectetur mollit.
+
+Veniam fugiat esse ad adipisicing fugiat deserunt ex dolore est aliqua. Aliqua velit eu enim cillum ullamco dolore. Sit eiusmod do ad cillum culpa id pariatur esse velit ex. Non ut culpa quis non esse minim ex Lorem sunt cillum incididunt. Proident excepteur nulla et velit aute.
+
+Mollit ea consequat esse deserunt sint dolor eu do dolor. Eu Lorem consectetur culpa culpa dolor reprehenderit pariatur cillum consequat ullamco id. Pariatur proident deserunt nulla ad culpa nulla duis ea. Dolore ut pariatur aliquip Lorem ullamco est ipsum anim magna cillum excepteur reprehenderit. Magna aute aliqua sit proident proident. Magna velit non ullamco sint excepteur laboris ullamco non laborum culpa laboris.
+
+Magna cillum ullamco exercitation cillum aliquip. Enim dolore officia aliqua incididunt cupidatat occaecat laborum laboris esse sunt enim cupidatat. Magna eiusmod ad magna ad tempor officia commodo fugiat Lorem non commodo dolore adipisicing occaecat. Incididunt velit nisi eu aliqua anim. Id excepteur adipisicing ea consequat ex culpa deserunt excepteur excepteur.
+
+Adipisicing Lorem ea mollit deserunt dolor laboris labore ea consectetur fugiat elit cupidatat adipisicing. Deserunt velit qui eiusmod esse voluptate proident eiusmod culpa ad mollit nulla. Deserunt dolor esse nisi proident tempor commodo fugiat Lorem ad adipisicing ipsum. Irure sint aute do non tempor et excepteur laborum pariatur deserunt et aute excepteur. Amet nulla eu deserunt eiusmod est consectetur quis fugiat consequat proident. Aute do ut amet aute velit duis proident.
+
+</p> -->
+</div>
+
+<style>
+    /* https://stackoverflow.com/a/26241019 :pray: */
+</style>
