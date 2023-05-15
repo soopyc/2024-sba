080077a66184eb308f8025ab729aff9245198fab
Cassie Cheung <me@soopy.moe>
2023-05-16 00:01:59 +0800

    feat: countdown timer and video fixup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,7 +16,7 @@ const config = {
       {
         'light': {
           "primary": "#3c9fce",
-          "secondary": "#6ec1e4",   
+          "secondary": "#6ec1e4",
           "accent": "#1d86a8",
           "neutral": "#000000",
           "base-100": "#ffffff",
@@ -24,7 +24,18 @@ const config = {
           "success": "#1f9352",
           "warning": "#ffa800",
           "error": "#eb2455",
-        }
+        },
+        'dark': {  // TODO: finish tuning dark mode color.
+            "primary": "#3c9fce",
+            "secondary": "#6ec1e4",
+            "accent": "#1d86a8",
+            "neutral": "#ffffff",
+            "base-100": "#000000",
+            "info": "#95e0f8",
+            "success": "#6fd392",
+            "warning": "#ffa800",
+            "error": "#eb2455",
+          }
       }
     ]
   }
