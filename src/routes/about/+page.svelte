a639e3cd39b66b74ad60721da63e50da51c8e34d
Cassie Cheung <me@soopy.moe>
2023-05-17 02:54:47 +0800

    wip: about page
    as of this commit, the following are completed:
    * hero title text and image
    
    these items are yet to be completed:
    * interactive contact form
    * actual about sections

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,69 @@
 <script>
+    import 'maplibre-gl/dist/maplibre-gl.css';
+    import maplibre from 'maplibre-gl'; // mfw 742.6k dep
     // https://maplibre.org/maplibre-gl-js-docs/api/
     // TODO: Setup this thing
     // and this thing https://tegola.io
 </script>
+
+<!-- Hero text about page and image -->
+<div class="hero min-h-[40vh] place-items-start" id="about-hero">
+    <div class="hero-overlay" />
+    <div class="hero-content text-left text-neutral-content justify-self-start self-end">
+        <div class="pl-10">
+            <!-- I wish I knew how to make this less hacky -->
+            <h1 class="text-5xl font-bold">About Us</h1>
+            <p class="py-4">Useful information about the school and the event.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Section 1: School location -->
+<div />
+
+<div>
+    Culpa id esse ex nulla proident consectetur id culpa consectetur ut excepteur. Aliquip elit voluptate sint occaecat
+    dolor ipsum velit do mollit in. Amet Lorem sit est non. In laboris cupidatat cillum anim amet qui sint consequat
+    officia tempor quis esse occaecat. Aliqua adipisicing irure ipsum ad nulla ullamco commodo dolor ut mollit. Minim ea
+    nostrud pariatur labore sunt nulla Lorem. Irure culpa laboris ullamco reprehenderit ex aliquip. Proident incididunt
+    eu commodo voluptate sit laboris et laboris exercitation tempor labore mollit quis exercitation. Fugiat minim Lorem
+    qui amet proident reprehenderit voluptate elit magna aliqua officia. Proident sit in culpa non cupidatat sint
+    adipisicing anim minim occaecat sunt ullamco aliquip. Non ullamco id velit duis occaecat culpa adipisicing culpa id
+    pariatur amet eu dolor. Lorem nisi enim culpa elit deserunt aliquip excepteur Lorem. Magna irure Lorem laboris
+    mollit incididunt ipsum do aute ipsum. Proident aute voluptate sint anim laborum occaecat minim. Id enim qui
+    proident qui. Eiusmod cillum cillum elit ad qui. Occaecat occaecat consectetur Lorem exercitation consectetur est
+    tempor do dolore cillum aute. Esse excepteur id ullamco voluptate cillum proident amet. Laborum officia nulla labore
+    reprehenderit ut. Excepteur elit tempor cupidatat ad elit do quis aute ex eu culpa consectetur consequat. Esse ea
+    adipisicing commodo adipisicing fugiat voluptate. Ea ad consectetur enim sint nulla dolor est. Lorem esse amet
+    exercitation voluptate laborum. Sit deserunt nostrud esse excepteur enim dolore laboris aute dolor cupidatat. Anim
+    qui commodo excepteur labore culpa reprehenderit velit in labore sint aliquip. Sit cupidatat exercitation fugiat et.
+    Fugiat magna ipsum sunt eiusmod proident quis enim ex mollit. Occaecat reprehenderit in veniam aliqua fugiat
+    reprehenderit nulla consequat dolore officia mollit. Est laboris nulla consectetur laborum irure nisi magna sit
+    ullamco Lorem cillum sunt. Adipisicing ad excepteur laboris ut nulla Lorem elit labore incididunt in. Laboris
+    occaecat deserunt cupidatat Lorem reprehenderit reprehenderit. Irure velit labore voluptate consequat aliquip
+    laboris nisi duis. Dolore laborum aute laborum adipisicing excepteur laborum reprehenderit eiusmod labore id elit
+    occaecat elit. Velit laborum eu magna nulla elit voluptate. Eiusmod consequat irure duis et ad incididunt. Cillum
+    duis ea cillum mollit magna ad commodo enim adipisicing. Elit ex aliqua ea voluptate sint cupidatat sunt
+    reprehenderit incididunt ullamco enim officia veniam officia. Consequat anim sint fugiat pariatur sunt deserunt do
+    cillum reprehenderit laboris magna laboris. Excepteur nisi officia sint nostrud sunt pariatur quis. Qui duis sunt
+    ullamco incididunt cillum magna. Eiusmod ex aliquip quis do exercitation anim qui tempor magna pariatur anim.
+    Voluptate incididunt magna laboris do eiusmod labore ad enim elit ullamco. Tempor do ad dolor consectetur nulla
+    occaecat velit ea. Tempor ullamco laborum occaecat adipisicing mollit occaecat fugiat laborum non esse qui fugiat
+    aute. Nisi esse duis quis magna. Aliquip minim veniam cupidatat elit mollit sit occaecat duis ad. Pariatur eiusmod
+    ipsum veniam aute magna officia eu do sunt magna ut consequat aliquip cupidatat. Aute cillum officia nulla et in ut
+    adipisicing id irure mollit elit duis amet excepteur. Cillum ut non aliqua do voluptate velit dolor aute esse magna
+    pariatur labore in. Eiusmod anim aliquip cupidatat consequat exercitation exercitation excepteur occaecat cupidatat
+    reprehenderit. Cupidatat in laborum dolore labore sint et eiusmod irure cupidatat magna culpa ullamco laboris.
+    Aliqua enim cupidatat veniam nulla exercitation non occaecat. Proident amet irure irure dolore anim. Occaecat nostrud in et excepteur et duis ipsum Lorem dolore quis consequat elit.
+    Ullamco pariatur anim sint consequat ex labore ea excepteur sint mollit velit minim dolor. Adipisicing consectetur excepteur veniam consectetur pariatur qui deserunt mollit magna fugiat occaecat reprehenderit commodo Lorem. Non sunt enim consectetur qui reprehenderit ea voluptate minim elit minim. Laborum nulla ad nisi commodo ipsum elit pariatur aliquip.
+</div>
+
+<!-- TODO: Section x: Contact form -->
+<!-- Requires server, will need to modify deployment code later. -->
+
+<style lang="scss">
+    #about-hero {
+        background-image: url('https://www.spkc.edu.hk/wp-content/uploads/2019/09/123-1024x348.jpg');
+        background-position: center;
+    }
+</style>
