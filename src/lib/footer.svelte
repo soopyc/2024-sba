95a24d51e456395548c142dc72cf302f99ad770d
Cassie Cheung <me@soopy.moe>
2023-11-01 01:50:00 +0800

    fix: fixup footer styling

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,20 +1,24 @@
 <script>
     import Fa from 'svelte-fa';
-    import { faPhone } from '@fortawesome/free-solid-svg-icons';
-    import Wireframe from '$lib/wireframe.svelte';
+    import { faPhone, faAt } from '@fortawesome/free-solid-svg-icons';
+    // import Wireframe from '$lib/wireframe.svelte';
 </script>
 
-<footer class="footer bg-accent text-neutral-content p-3 lg:p-5 lg:px-10 lg:text-xl">
-    <div class="flex place-items-center">
-        <Wireframe width="50px" height="50px" />
+<footer class="footer bg-accent text-neutral-content p-3 lg:p-5 lg:px-10 lg:text-xl flex items-center justify-around">
+    <div class="flex items-center">
+        <!-- <img alt="School badge of SPKC" src="/schoolbadge.png" height="50px" width="50px" /> -->
         <p>&copy; Stewards Pooi Kei College 2023</p>
     </div>
 
-    <div>
-        <div class="flex gap-2 ">
-            <a href="tel:+852-2635-0100">
-                <Fa icon={faPhone} />
-            </a>
-        </div>
+		<div class="flex gap-2">
+			<!-- <a href="tel:+852-2635-0100"> ????? -->
+			<a href="tel:+852-2345-4567" class="flex items-center gap-2">
+					<Fa icon={faPhone} />
+					<span>2345 4567</span>
+			</a>
+			<a href="mailto:info@spkc.edu.hk" class="flex items-center gap-2">
+					<Fa icon={faAt} />
+					<span>info@spkc.edu.hk</span>
+			</a>
     </div>
 </footer>
