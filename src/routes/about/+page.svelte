0ace2391bfe628bba2a36c0b765d15db249d4fdd
Cassie Cheung <me@soopy.moe>
2023-11-01 01:47:08 +0800

    feat: add school location embed

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,13 +1,10 @@
 <script>
-    import 'maplibre-gl/dist/maplibre-gl.css';
-    import maplibre from 'maplibre-gl'; // mfw 742.6k dep
-    // https://maplibre.org/maplibre-gl-js-docs/api/
-    // TODO: Setup this thing
-    // and this thing https://tegola.io
+	import { PUBLIC_GCP_API_KEY } from "$env/static/public";
+	export const LOC = "Stewards Pooi Kei College, 56 Siu Lek Yuen Road, Sha Tin"
 </script>
 
 <!-- Hero text about page and image -->
-<div class="hero min-h-[40vh] place-items-start" id="about-hero">
+<div class="hero min-h-[40vh] place-items-start mb-4" id="about-hero">
     <div class="hero-overlay" />
     <div class="hero-content text-left text-neutral-content justify-self-start self-end">
         <div class="pl-10">
@@ -19,10 +16,20 @@
 </div>
 
 <!-- Section 1: School location -->
-<div />
 
-<div class="mx-5">
-    Lorem ipsum dolor sit amet. 
+<div class="max-w-screen-xl mx-auto">
+	<div>
+		<h2 class="text-3xl md-2">School Location</h2>
+		<iframe
+			loading="lazy"
+			referrerpolicy="no-referrer-when-downgrade"
+			title="GMaps embed for the location of SPKC."
+			src="https://www.google.com/maps/embed/v1/place?key={PUBLIC_GCP_API_KEY}&q={encodeURI(LOC)}"
+			class="border-none w-full min-h-[25em]"
+		/>
+	</div>
+
+    Lorem ipsum dolor sit amet.
 
     Culpa id esse ex nulla proident consectetur id culpa consectetur ut excepteur. Aliquip elit voluptate sint occaecat
     dolor ipsum velit do mollit in. Amet Lorem sit est non. In laboris cupidatat cillum anim amet qui sint consequat
@@ -30,34 +37,7 @@
     nostrud pariatur labore sunt nulla Lorem. Irure culpa laboris ullamco reprehenderit ex aliquip. Proident incididunt
     eu commodo voluptate sit laboris et laboris exercitation tempor labore mollit quis exercitation. Fugiat minim Lorem
     qui amet proident reprehenderit voluptate elit magna aliqua officia. Proident sit in culpa non cupidatat sint
-    adipisicing anim minim occaecat sunt ullamco aliquip. Non ullamco id velit duis occaecat culpa adipisicing culpa id
-    pariatur amet eu dolor. Lorem nisi enim culpa elit deserunt aliquip excepteur Lorem. Magna irure Lorem laboris
-    mollit incididunt ipsum do aute ipsum. Proident aute voluptate sint anim laborum occaecat minim. Id enim qui
-    proident qui. Eiusmod cillum cillum elit ad qui. Occaecat occaecat consectetur Lorem exercitation consectetur est
-    tempor do dolore cillum aute. Esse excepteur id ullamco voluptate cillum proident amet. Laborum officia nulla labore
-    reprehenderit ut. Excepteur elit tempor cupidatat ad elit do quis aute ex eu culpa consectetur consequat. Esse ea
-    adipisicing commodo adipisicing fugiat voluptate. Ea ad consectetur enim sint nulla dolor est. Lorem esse amet
-    exercitation voluptate laborum. Sit deserunt nostrud esse excepteur enim dolore laboris aute dolor cupidatat. Anim
-    qui commodo excepteur labore culpa reprehenderit velit in labore sint aliquip. Sit cupidatat exercitation fugiat et.
-    Fugiat magna ipsum sunt eiusmod proident quis enim ex mollit. Occaecat reprehenderit in veniam aliqua fugiat
-    reprehenderit nulla consequat dolore officia mollit. Est laboris nulla consectetur laborum irure nisi magna sit
-    ullamco Lorem cillum sunt. Adipisicing ad excepteur laboris ut nulla Lorem elit labore incididunt in. Laboris
-    occaecat deserunt cupidatat Lorem reprehenderit reprehenderit. Irure velit labore voluptate consequat aliquip
-    laboris nisi duis. Dolore laborum aute laborum adipisicing excepteur laborum reprehenderit eiusmod labore id elit
-    occaecat elit. Velit laborum eu magna nulla elit voluptate. Eiusmod consequat irure duis et ad incididunt. Cillum
-    duis ea cillum mollit magna ad commodo enim adipisicing. Elit ex aliqua ea voluptate sint cupidatat sunt
-    reprehenderit incididunt ullamco enim officia veniam officia. Consequat anim sint fugiat pariatur sunt deserunt do
-    cillum reprehenderit laboris magna laboris. Excepteur nisi officia sint nostrud sunt pariatur quis. Qui duis sunt
-    ullamco incididunt cillum magna. Eiusmod ex aliquip quis do exercitation anim qui tempor magna pariatur anim.
-    Voluptate incididunt magna laboris do eiusmod labore ad enim elit ullamco. Tempor do ad dolor consectetur nulla
-    occaecat velit ea. Tempor ullamco laborum occaecat adipisicing mollit occaecat fugiat laborum non esse qui fugiat
-    aute. Nisi esse duis quis magna. Aliquip minim veniam cupidatat elit mollit sit occaecat duis ad. Pariatur eiusmod
-    ipsum veniam aute magna officia eu do sunt magna ut consequat aliquip cupidatat. Aute cillum officia nulla et in ut
-    adipisicing id irure mollit elit duis amet excepteur. Cillum ut non aliqua do voluptate velit dolor aute esse magna
-    pariatur labore in. Eiusmod anim aliquip cupidatat consequat exercitation exercitation excepteur occaecat cupidatat
-    reprehenderit. Cupidatat in laborum dolore labore sint et eiusmod irure cupidatat magna culpa ullamco laboris.
-    Aliqua enim cupidatat veniam nulla exercitation non occaecat. Proident amet irure irure dolore anim. Occaecat nostrud in et excepteur et duis ipsum Lorem dolore quis consequat elit.
-    Ullamco pariatur anim sint consequat ex labore ea excepteur sint mollit velit minim dolor. Adipisicing consectetur excepteur veniam consectetur pariatur qui deserunt mollit magna fugiat occaecat reprehenderit commodo Lorem. Non sunt enim consectetur qui reprehenderit ea voluptate minim elit minim. Laborum nulla ad nisi commodo ipsum elit pariatur aliquip.
+    adipisicing anim minim occaecat sunt ullamco aliquip.
 </div>
 
 <!-- TODO: Section x: Contact form -->
