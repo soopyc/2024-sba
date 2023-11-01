8253827362128ef0b2ee2afe42a5ecace1af3152
Cassie Cheung <me@soopy.moe>
2023-11-01 19:14:53 +0800

    style: enforce formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,90 +1,90 @@
 <script>
-    import '../app.postcss';
-    import '../app.sass';
+	import '../app.postcss';
+	import '../app.sass';
 
-    import Fa from 'svelte-fa';
-    import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+	import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
-    import { page } from '$app/stores';
-    import Footer from '$lib/footer.svelte';
+	import { page } from '$app/stores';
+	import Footer from '$lib/footer.svelte';
 </script>
 
 <svelte:head>
-    <title>20th Anniversary | Stewards Pooi Kei College</title>
+	<title>20th Anniversary | Stewards Pooi Kei College</title>
+
+	<meta
+		name="description"
+		content="Established in 2004, SPKC is a secondary school in Shatin, Hong Kong. Join us to celebrate our school's 20th anniversary with love and glee."
+	/>
+	<meta name="keywords" content="SPKC, Stewards Pooi Kei College, 20th anniversary, anniversary" />
+	<meta name="robots" content="none" />
 </svelte:head>
 
 <div class="drawer">
-    <input id="main-drawer" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content flex flex-col">
-        <!-- navbar -->
-        <div class="navbar serif bg-primary shadow sticky top-0 min-h-fit lg:px-10 z-10">
-            <div class="flex-auto m-auto">
-                <div class="flex-none lg:hidden mx-3" title="Site navigation drawer">
-                    <label for="main-drawer" class="btn btn-square btn-ghost">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            class="inline-block w-6 h-6 stroke-current"
-                            ><path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16"
-                            /></svg
-                        >
-                    </label>
-                </div>
+	<input id="main-drawer" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content flex flex-col">
+		<!-- navbar -->
+		<div class="navbar serif bg-primary shadow sticky top-0 min-h-fit lg:px-10 z-10">
+			<div class="flex-auto m-auto">
+				<div class="flex-none lg:hidden mx-3" title="Site navigation drawer">
+					<label for="main-drawer" class="btn btn-square btn-ghost">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							class="inline-block w-6 h-6 stroke-current"
+							><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /></svg
+						>
+					</label>
+				</div>
 
-                <a href="/" class="flex justify-around space-x-3">
-                    <img class="h-14" alt="Stewards Pooi Kei College 20th Anniversary" src="/schoolbadge.png" />
-                    <div class="text-left flex flex-col flex-auto justify-center items-start">
-                        <p>
-                            <span class="hidden lg:inline">香港神托會</span>培基書院
-                            <span class="lg:text-xl">20週年</span>
-                        </p>
-                        <p>
-                            <span class="lg:hidden">SPKC</span>
-                            <span class="hidden lg:inline lg:text-xl">Stewards Pooi Kei College</span>
-                            <span class="lg:text-xl">20th Anniversary</span>
-                        </p>
-                    </div>
-                </a>
-            </div>
+				<a href="/" class="flex justify-around space-x-3">
+					<img class="h-14" alt="Stewards Pooi Kei College 20th Anniversary" src="/schoolbadge.png" />
+					<div class="text-left flex flex-col flex-auto justify-center items-start">
+						<p>
+							<span class="hidden lg:inline">香港神托會</span>培基書院
+							<span class="lg:text-xl">20週年</span>
+						</p>
+						<p>
+							<span class="lg:hidden">SPKC</span>
+							<span class="hidden lg:inline lg:text-xl">Stewards Pooi Kei College</span>
+							<span class="lg:text-xl">20th Anniversary</span>
+						</p>
+					</div>
+				</a>
+			</div>
 
-            <div class="flex-none hidden lg:block">
-                <ul class="menu menu-horizontal bg-secondary rounded-md">
-                    <!-- todo: rewrite these with components for auto styling -->
-                    <li><a href="/" class:bg-info={$page.url.pathname === '/'}>Home</a></li>
-                    <li><a href="/activities" class:bg-info={$page.url.pathname === '/activities'}>Activities</a></li>
-                    <li><a href="/about" class:bg-info={$page.url.pathname === '/about'}>About Us</a></li>
-                    <li>
-                        <a class="hover-bordered bg-warning hover:bg-warning/70" href="//www.spkc.edu.hk">
-                            Return to Main Site
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+			<div class="flex-none hidden lg:block">
+				<ul class="menu menu-horizontal bg-secondary rounded-md">
+					<!-- todo: rewrite these with components for auto styling -->
+					<li><a href="/" class:bg-info={$page.url.pathname === '/'}>Home</a></li>
+					<li><a href="/activities" class:bg-info={$page.url.pathname === '/activities'}>Activities</a></li>
+					<li><a href="/about" class:bg-info={$page.url.pathname === '/about'}>About Us</a></li>
+					<li>
+						<a class="hover-bordered bg-warning hover:bg-warning/70" href="//www.spkc.edu.hk"> Return to Main Site </a>
+					</li>
+				</ul>
+			</div>
+		</div>
 
-        <!-- Main content -->
-        <slot />
-        <Footer />
-    </div>
+		<!-- Main content -->
+		<slot />
+		<Footer />
+	</div>
 
-    <!-- Sidebar -->
-    <div class="drawer-side">
-        <label for="main-drawer" class="drawer-overlay" />
-        <ul class="menu p-4 w-56 md:w-80 bg-base-100 md:gap-2 text-sm md:text-base">
-            <li class="menu-title"><span>Site navigation</span></li>
-            <li><a href="/" class:bg-info={$page.url.pathname === '/'}>Home</a></li>
-            <li><a href="/activities" class:bg-info={$page.url.pathname === '/activities'}>Activities</a></li>
-            <li><a href="/about" class:bg-info={$page.url.pathname === '/about'}>About Us</a></li>
-            <li>
-                <a class="hover-bordered bg-warning hover:bg-warning/70" href="//www.spkc.edu.hk">
-                    <Fa icon={faRightFromBracket} />Return to Main Site
-                </a>
-            </li>
-        </ul>
-    </div>
+	<!-- Sidebar -->
+	<div class="drawer-side">
+		<label for="main-drawer" class="drawer-overlay" />
+		<ul class="menu p-4 w-56 md:w-80 bg-base-100 md:gap-2 text-sm md:text-base">
+			<li class="menu-title"><span>Site navigation</span></li>
+			<li><a href="/" class:bg-info={$page.url.pathname === '/'}>Home</a></li>
+			<li><a href="/activities" class:bg-info={$page.url.pathname === '/activities'}>Activities</a></li>
+			<li><a href="/about" class:bg-info={$page.url.pathname === '/about'}>About Us</a></li>
+			<li>
+				<a class="hover-bordered bg-warning hover:bg-warning/70" href="//www.spkc.edu.hk">
+					<Fa icon={faRightFromBracket} />Return to Main Site
+				</a>
+			</li>
+		</ul>
+	</div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,44 +1,43 @@
 const config = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {}
-  },
+	theme: {
+		extend: {},
+	},
 
-  plugins: [
-    require('daisyui')
-  ],
+	plugins: [require('daisyui')],
 
-  daisyui: {
-    base: true,
-    logs: false,
-    themes: [
-      {
-        'light': {
-          "primary": "#3c9fce",
-          "secondary": "#6ec1e4",
-          "accent": "#1d86a8",
-          "neutral": "#000000",
-          "base-100": "#ffffff",
-          "info": "#95e0f8",
-          "success": "#1f9352",
-          "warning": "#ffa800",
-          "error": "#eb2455",
-        },
-        'dark': {  // TODO: finish tuning dark mode color.
-            "primary": "#3c9fce",
-            "secondary": "#6ec1e4",
-            "accent": "#1d86a8",
-            "neutral": "#ffffff",
-            "base-100": "#000000",
-            "info": "#95e0f8",
-            "success": "#6fd392",
-            "warning": "#ffa800",
-            "error": "#eb2455",
-          }
-      }
-    ]
-  }
+	daisyui: {
+		base: true,
+		logs: false,
+		themes: [
+			{
+				light: {
+					primary: '#3c9fce',
+					secondary: '#6ec1e4',
+					accent: '#1d86a8',
+					neutral: '#000000',
+					'base-100': '#ffffff',
+					info: '#95e0f8',
+					success: '#1f9352',
+					warning: '#ffa800',
+					error: '#eb2455',
+				},
+				dark: {
+					// TODO: finish tuning dark mode color.
+					primary: '#3c9fce',
+					secondary: '#6ec1e4',
+					accent: '#1d86a8',
+					neutral: '#ffffff',
+					'base-100': '#000000',
+					info: '#95e0f8',
+					success: '#6fd392',
+					warning: '#ffa800',
+					error: '#eb2455',
+				},
+			},
+		],
+	},
 };
 
 module.exports = config;
